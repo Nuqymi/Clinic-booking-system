34516afd01e9dc4edf8c1b48b1c770f37fc6b9e0
Nuqymi <nuqimi0@yahoo.com>
2025-04-19 13:34:26 +0800

    Delete clinic/clinic directory

--- clinic/clinic/src/main/java/com/project/clinic/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.clinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.clinic.model.Doctor;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
-
